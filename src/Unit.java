0eab3a9f7dacced141357c76f8fb2c3387112276
erinhill25 <erinhill25@charter.net>
2014-11-13 12:47:59 -0600

    Avery Destroyed Everything we Cared about

--- src/Unit.java
+++ src/Unit.java
@@ -1,7 +1,23 @@
+import java.awt.Graphics2D;
 
-public class Unit {
 
+public class Unit extends Entity {
+
+	Player owner;
+	int movesRemaining, possibleMoves, active;
 	
+	public Unit(Player owner) {
+		this.owner = owner;
+	}
 	
-	
+	@Override
+	void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	void update() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/UnitHandler.java
+++ src/UnitHandler.java
@@ -1,4 +1,14 @@
 
 public class UnitHandler {
-
+	Unit[] player1Units;
+	Unit[] player2Units;
+	
+	public UnitHandler() {
+		player1Units = new Unit[11];
+		player2Units = new Unit[11];
+	}
+	
+	public void addUnit(Unit unit) {
+		
+	}
 }
